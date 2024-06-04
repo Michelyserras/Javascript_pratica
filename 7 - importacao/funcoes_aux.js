6712a7738388d8f40f5bdff12c45fcb82011aff9
Michelyserras <Michelysrrs@gmail.com>
2024-06-03 21:29:51 -0300

    importacao - require e module.exports

---
+++ 7 - importacao/funcoes_aux.js
@@ -0,0 +1,16 @@
+
+
+function gets() {
+    return 10;
+
+}
+
+function print(texto) {
+    console.log(texto);
+}
+
+module.exports = { gets, print};
+
+//outro jeito
+//module.export.gets = gets
+//mocked == simulação
---
+++ 7 - importacao/main.js
@@ -0,0 +1,15 @@
+//importando funções
+//const funcoes = require('./funcoes_aux');
+
+const { gets, print }= require('./funcoes_aux');
+
+/*const pessoa = {
+    nome: 'vitor'
+};
+
+// esses dois códigos são equivalente
+// destructing
+const { nome } = pessoa;
+const nome = pessoa.nome;
+*/
+print(gets());
