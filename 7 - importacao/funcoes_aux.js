77e48e67d68721e112eb9398bc582a00a5ef6be5
Michelyserras <Michelysrrs@gmail.com>
2024-06-04 21:41:51 -0300

    importacao - ex de numeros sorteados

--- 7 - importacao/funcoes_aux.js
+++ 7 - importacao/funcoes_aux.js
@@ -1,7 +1,11 @@
 
+const entradas = [5, 50, 10, 98, 23, 99, 100];
+let i = 0; 
 
 function gets() {
-    return 10;
+    const valor = entradas[i];
+    i++;
+    return valor;
 
 }
 
@@ -14,3 +18,41 @@ module.exports = { gets, print};
 //outro jeito
 //module.export.gets = gets
 //mocked == simulação
+
+/*
+Como é o ciclo desse código? como ele funciona?
+R: Primeiro o node.js irá executar as funções e em seguida preparar elas para serem exportadas,
+ após isso entende-se que o arquivo funcoes_aux esta exportando as funçoes gets e print
+ */
+
+/*const alunos = [];
+
+ function sorteio(numero){
+   return alunos.push(numero);
+ }
+
+ function maiorNum(){
+    let maior = 0;
+    for(let i = 0; i <= entradas.length; i++){
+        if(entradas[i] >= maior)
+            maior = entradas[i];
+            
+    }
+    return maior;
+ }
+
+/*
+console.log('Inicio do sorteio', alunos);
+console.log('tamanho da lista: ', alunos.length);
+
+ sorteio(5);
+ sorteio(50);
+ sorteio(10);
+ sorteio(98);
+ sorteio(23);
+
+ console.log('Numeros sorteados: \n',alunos);
+
+console.log('O maior numero sorteado foi: ', maiorNum(alunos));
+console.log('tamanho da lista: ', alunos.length);
+*/
--- 7 - importacao/main.js
+++ 7 - importacao/main.js
@@ -1,7 +1,9 @@
 //importando funções
 //const funcoes = require('./funcoes_aux');
 
-const { gets, print }= require('./funcoes_aux');
+const { gets, print}= require('./funcoes_aux');
+
+const qtd = gets();
 
 /*const pessoa = {
     nome: 'vitor'
@@ -12,4 +14,34 @@ const { gets, print }= require('./funcoes_aux');
 const { nome } = pessoa;
 const nome = pessoa.nome;
 */
-print(gets());
+
+/*const numSorteados = [];
+
+for(let i = 0; i<5; i++){
+   const numerosSorteado = gets();
+   numSorteados.push(numerosSorteado);
+}
+
+print(numSorteados);
+*/
+let maior = 0;
+/*
+for(let i=0; i<=numSorteados.length; i++){
+    if(numSorteados[i] > maior)
+        maior = numSorteados[i];
+}
+
+*/
+// outra forma
+
+
+for(let i = 0; i <= qtd; i++){
+    const numerosSorteado = gets();
+    if(numerosSorteado > maior){
+        maior = numerosSorteado;
+    }
+}
+
+print(maior);
+
+
